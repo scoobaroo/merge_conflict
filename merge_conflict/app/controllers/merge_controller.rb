2db00ab557026cc07166b1dd0dbfa73bfaee4092
scoobaroo <scoobaroo415@gmail.com>
2016-05-05 10:13:51 -0700

    made better readme and controller methods

--- merge_conflict/app/controllers/merge_controller.rb
+++ merge_conflict/app/controllers/merge_controller.rb
@@ -1,4 +1,11 @@
 class MergeController < ApplicationController
   def merge_my_ass
+    p "merge my ass plsssss!"
+  end
+  def merge_me
+    puts "merge me!"
+  end
+  def merge_surge
+    p "merge surge baby!"
   end
 end
